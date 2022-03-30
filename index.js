b3be4b16a212fb9250a68c3d56d7025abab05467
Michael Caley <kneedeepwater@hotmail.com>
2022-03-30 21:04:05 +0100

    add aleready posted feedback

--- index.js
+++ index.js
@@ -12,13 +12,16 @@ config();
 async function twitterYoutubePoster() {
   try {
     const uploadId = await fetchId();
-    const uploadList = await fetchUploadList(uploadId);
-    const ytPublishedAt = uploadList[0].publishedAt;
+    const [lastUpload] = await fetchUploadList(uploadId);
+    const ytPublishedAt = lastUpload.publishedAt;
     const lastPublished = await readLastPublished();
     const hasAlreadyPosted = ytPublishedAt === lastPublished;
-    if (hasAlreadyPosted) return;
+    if (hasAlreadyPosted) {
+      console.log(`LOG: hasAlreadyPosted`);
+      return;
+    }
     writeLastPublished(ytPublishedAt);
-    const { title, videoId } = uploadList[0];
+    const { title, videoId } = lastUpload;
     postTweet(`${title} https://youtu.be/${videoId}`);
   } catch (e) {
     // eslint-disable-next-line no-console
